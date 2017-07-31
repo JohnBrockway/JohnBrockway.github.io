9d89ae813e12a5c19511e56ed89106bfae5f4d42
John Brockway <jbrockway@spscommerce.com>
2017-07-31 14:45:07 -0400

    Make indents consistent

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,24 +1,24 @@
 function operateNav() {
-	if (document.getElementById("sidenav").style.width > "0px") {
-		closeNav();
-	}
-	else {
-		openNav();
-	}
+    if (document.getElementById("sidenav").style.width > "0px") {
+        closeNav();
+    }
+    else {
+        openNav();
+    }
 }
 
 function openNav() {
     document.getElementById("sidenav").style.width = "15%";
     document.getElementById("main").style.marginLeft = "15%";
-	document.getElementById("main").style.width = "85%";
-	document.getElementById("header").style.width = "85%";
-	document.getElementById("openAndCloseNav").innerHTML="&times;";
+    document.getElementById("main").style.width = "85%";
+    document.getElementById("header").style.width = "85%";
+    document.getElementById("openAndCloseNav").innerHTML="&times;";
 }
 
 function closeNav() {
     document.getElementById("sidenav").style.width = "0px";
-	document.getElementById("main").style.width = "100%";
-	document.getElementById("header").style.width = "100%";
+    document.getElementById("main").style.width = "100%";
+    document.getElementById("header").style.width = "100%";
     document.getElementById("main").style.marginLeft = "0px";
-	document.getElementById("openAndCloseNav").innerHTML="&#x2261;";
+    document.getElementById("openAndCloseNav").innerHTML="&#x2261;";
 }
